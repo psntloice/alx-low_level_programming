cbae0e74b738df67ed95bff3986fa47393b7341c
unknown <nyandiakamau24@gmail.com>
2022-05-06 12:15:47 +0200

    file add

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void)
+{
+printf("Size of a char: %lu", sizeof(char))
+printf("Size of an int: %lu", sizeof(int))
+printf("Size of a long int: %lu", sizeof(long int))
+printf("Size of a long long int: %lu", sizeof(long long int))
+printf("Size of a float: %lu", sizeof(float))
+
+return(0)
+}
